edc4ac3cfef56b3aef66e69f17a34d7485cb3f07
Hmvalles75 <51212330+Hmvalles75@users.noreply.github.com>
2019-08-30 08:17:23 -0700

    dummy store

---
+++ src/store.js
@@ -0,0 +1,19 @@
+const uuid = require('uuid/v4')
+
+const bookmarks = [
+  { id: uuid(),
+    title: 'webpage1',
+    url: 'https://www.google.com',
+    description: 'loerm ipsum nopam',
+    rating: 5 },
+  { id: uuid(),
+    title: 'webpage2',
+    url: 'https://www.instagram.com',
+    description: 'picture sharing',
+    rating: 3 },
+  { id: uuid(),
+    title: 'website3',
+    url: 'https://thinkful.com',
+    description: 'coding bootcamp',
+    rating: 4 },
+]
